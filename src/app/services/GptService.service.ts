9318818d7a03f4ca923d961a49a12252645ba766
Paulo Fernandes <paulor.exe@gmail.com>
2025-09-17 15:32:13 -0300

    feat: adiciona fluxo de sinusite ao seio frontal masc e fem. Adiciona gptKey como variavel de ambiente

--- src/app/services/GptService.service.ts
+++ src/app/services/GptService.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
+import { environment } from '../../environments/environment'
 
 @Injectable({
   providedIn: 'root',
 })
 export class GptServiceService {
   private apiUrl = 'https://api.openai.com/v1/chat/completions'
-  private apiKey = ''
+  private apiKey = environment.gptKey
 
   constructor(private http: HttpClient) {}
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,4 +8,6 @@ export const environment = {
     appId: '',
     measurementId: '',
   },
+  production: false,
+  gptKey: '',
 }
